a8c9c5499eafdb91373410be7fc1062118104698
Elías Alonso <eliasagc@gmail.com>
2012-03-08 17:05:35 +0100

    finishing touches on demo_2

--- demo/demo_2.js.coffee
+++ demo/demo_2.js.coffee
@@ -1,8 +1,4 @@
-# Tag Input: take 2
-#
-# Event Map:
-#  - TagAccumulator: in = [delete-last, store], out = []
-#  - TagSuggest: in = [suggest, go-up, go-down, select-suggestion, cancel-selection], out = [selected]
+## Tag Input Widget: Take 2
 
 # Channel definition (pub/sub)
 
@@ -13,7 +9,6 @@ class Channel
   unsubscribe: (chn, fn) ->
     # pending
   publish: (type, args...) ->
-    console.log 'published: ' + type
     unless @events[type] then return false
     for subscriber in @events[type]
       subscriber.callback.apply(subscriber.context, args)
@@ -25,28 +20,16 @@ class Channel
 class TagAcumulator
   constructor: (ch) ->
     @channel = ch
-    @channel.subscribe 'store', @store, @
-    @channel.subscribe 'delete-last', @delete_last, @
+    # input
+    @channel.subscribe 'accumulator:store', @store, @
+    @channel.subscribe 'accumulator:delete-last', @delete_last, @
+    # state
     @stored_tags = []
+    # initialize
     @dom = @initialize_dom()
     @bind_events(@dom)
 
-  initialize_dom: ->
-    tag_list: $('#demo2-tag-list')
-
-  bind_events: (dom) ->
-    self = @
-    dom.tag_list.delegate 'li.e-tag > a', 'click', (e) ->
-      tag_element = $(this).closest('li.e-tag')
-      self.stored_tags = _.without self.stored_tags, tag_element.attr('data-tag')
-      tag_element.remove()
-
-  render: (tag) ->
-    $('<li/>')
-      .addClass('e-tag')
-      .attr('data-tag', tag)
-      .text(tag)
-      .append('<a href="#">x</a>')
+  # Operations
 
   store: (tag) ->
     @stored_tags.push tag
@@ -56,22 +39,45 @@ class TagAcumulator
     deleted = @stored_tags.pop()
     @dom.tag_list.find("[data-tag=#{deleted}]").remove()
 
+  delete_tag: (tag) ->
+    @stored_tags = _.without @stored_tags, tag
+
+  # DOM
+
+  initialize_dom: ->
+    tag_list: $('#demo2-tag-list')
+
+  bind_events: (dom) ->
+    self = @
+    dom.tag_list.delegate 'li.e-tag > a', 'click', (e) ->
+      tag_element = $(this).closest('li.e-tag')
+      self.delete_tag tag_element.attr('data-tag')
+      tag_element.remove()
+
+  render: (tag) ->
+    $('<li/>')
+      .addClass('e-tag')
+      .attr('data-tag', tag)
+      .text(tag)
+      .append('<a href="#">x</a>')
+
 # Show and manage the suggestion box
 
 class TagSuggestion
   constructor: (ch) ->
     @channel = ch
-    @channel.subscribe 'suggest', @suggest, @
-    @channel.subscribe 'end-suggest', @close_box, @
-    @channel.subscribe 'go-down', @go_down, @
-    @channel.subscribe 'go-up', @go_up, @
-    @channel.subscribe 'select-suggestion', @select_suggestion, @
-    @channel.subscribe 'cancel-selection', @cancel_selection, @
-    @input = null
-    @selected_index = 0
-    @box_active = false
-    @suggestion_list = ["hola", "hello", "ahoy!", "bonjorno", "halo", "buenas", "akandemorl", "jarl"]
-    @active_suggestion_list = []
+    # input
+    @channel.subscribe 'suggest:show',   @show,    @
+    @channel.subscribe 'suggest:close',  @close,   @
+    @channel.subscribe 'suggest:next',   @go_next, @
+    @channel.subscribe 'suggest:prev',   @go_prev, @
+    @channel.subscribe 'suggest:select', @select,  @
+    # state
+    @input                  = null
+    @selected_index         = 0
+    @available_values       = ["hola", "hello", "ahoy!", "bonjorno", "halo", "buenas", "akandemorl", "jarl"]
+    @options_list           = []
+    #init
     @dom = @initialize_dom()
     @bind_events(@dom)
 
@@ -82,54 +88,56 @@ class TagSuggestion
     self = @
     dom.suggestion_box.delegate 'li.e-suggestion', 'click', ->
       suggestion = $(this).attr('data-suggestion')
-      self.channel.publish('selected', suggestion)
+      self.select_suggestion(suggestion)
 
-  suggest: (input) ->
+  show: (input) ->
     # Don't do anything if the input is the same
-    if @input != input
-      @input = input
-      return @dom.suggestion_box.slideUp() if input.length == 0
-      @active_suggestion_list = _.filter @suggestion_list, (w) -> w.match(input)
-      if @active_suggestion_list.length > 0
-        @dom.suggestion_box.html('')
-        for s in @active_suggestion_list
-          @dom.suggestion_box.append @render(s)
-        @dom.suggestion_box.slideDown()
-        @selected_index = -1
-      else
-        @dom.suggestion_box.slideUp()
+    return unless @input != input
+    return @dom.suggestion_box.slideUp() if input.length == 0
+    self = @
+    @input = input
+    @options_list = _.filter @available_values, (w) -> w.match(input)
+    if @options_list.length > 0
+      @dom.suggestion_box.html('')
+      _.each @options_list, (s) -> self.dom.suggestion_box.append(self.render(s))
+      @dom.suggestion_box.slideDown()
+      @selected_index = -1
+    else
+      @dom.suggestion_box.slideUp()
 
   render: (s) ->
-    $('<li/>')
-      .addClass('e-suggestion')
-      .attr('data-suggestion', s)
-      .text(s)
+    $('<li/>').addClass('e-suggestion').attr('data-suggestion', s).text(s)
 
-  close_box: ->
+  close: ->
     @dom.suggestion_box.hide()
 
-  go_down: ->
-    # increase the selection_index and highlight
+  go_next: ->
+    @selected_index = Math.min(@options_list.length - 1, @selected_index + 1)
+    @highlight_element()
 
-  go_up: ->
-    # decrease the selection index and highlight
+  go_prev: ->
+    @selected_index = Math.max(0, @selected_index - 1)
+    @highlight_element()
 
   highlight_element: (n) ->
+    n ||= @selected_index
+    @dom.suggestion_box.find('li').css('color', '')
+    @dom.suggestion_box.find("li:nth-child(#{n+1})").css('color', 'red')
 
-  select_suggestion: ->
+  select: (selected) ->
     # select currently highlighted suggestion
+    selected ||= @options_list[@selected_index]
+    @channel.publish('suggest:selected', selected)
+    @close_box()
 
-  cancel_selection: ->
-    # suspend the motion/selection events
-
-# Widget parent class
+# Input Dispatcher
 
 class TagInput
-  constructor: ->
-    @channel = new Channel()
-    @accumulator = new TagAcumulator(@channel)
-    @suggest = new TagSuggestion(@channel)
-    @channel.subscribe 'selected', @selected_suggestion, @
+  constructor: (ch) ->
+    @channel = ch
+    #input
+    @channel.subscribe 'input:clear', (-> @dom.input.val('')), @
+    # init
     @dom = @initialize_dom()
     @bind_events(@dom)
 
@@ -137,26 +145,39 @@ class TagInput
       input: $('#demo2-input')
 
   bind_events: (dom) ->
+    # Dispatch events based on keypresses
     self = @
     dom.input.keyup (e) ->
+      value = self.dom.input.val()
       switch e.keyCode
-        when 40     then self.channel.publish('go-down')
-        when 38     then self.channel.publish('go-up')
-        when 32,188 then e.preventDefault(); self.read_input()
-        when 13     then self.channel.publish('select-suggestion')
-        when 8      then self.channel.publish('delete-last')
-        when 27     then self.channel.publish('cancel-selection')
-      self.channel.publish('suggest', self.dom.input.val())
+        when 40     then self.channel.publish('suggest:next')
+        when 38     then self.channel.publish('suggest:prev')
+        when 32,188 then self.channel.publish('input:tag', value)
+        when 13     then self.channel.publish('suggest:select')
+        when 8      then self.channel.publish('accumulator:delete-last') if value.length == 0
+      self.channel.publish('suggest:show', value)
 
-  read_input: (input) ->
-    value = input || @dom.input.val()
-    value = value.trim().replace(/\W/g, '')
-    @channel.publish('store', value) unless value.length <= 0
-    @channel.publish('end-suggest')
-    @dom.input.val('')
+# Parent Widget
+
+class TagWidget
+  constructor: ->
+    # the decoupled components
+    @channel     = new Channel()
+    @input       = new TagInput(@channel)
+    @accumulator = new TagAcumulator(@channel)
+    @suggest     = new TagSuggestion(@channel)
+    # input
+    @channel.subscribe 'suggest:selected', @read_tag, @
+    @channel.subscribe 'input:tag',        @read_tag, @
+
+  read_tag: (input) ->
+    value = input.trim().replace(/\W/g, '')
+    # coordine the action
+    @channel.publish('accumulator:store', value) unless value.length <= 0
+    @channel.publish('suggest:close')
+    @channel.publish('input:clear')
 
   selected_suggestion: (suggestion) ->
     @read_input(suggestion)
 
-$ ->
-  window.tag_input = new TagInput()
+$ -> window.tag_input = new TagWidget()
--- demo/demo_2.js.js
+++ demo/demo_2.js.js
@@ -1,5 +1,5 @@
 (function() {
-  var Channel, TagAcumulator, TagInput, TagSuggestion;
+  var Channel, TagAcumulator, TagInput, TagSuggestion, TagWidget;
   var __slice = Array.prototype.slice;
   Channel = (function() {
     Channel.prototype.subscribe = function(type, fn, ctx) {
@@ -14,7 +14,6 @@
     Channel.prototype.publish = function() {
       var args, subscriber, type, _i, _len, _ref, _results;
       type = arguments[0], args = 2 <= arguments.length ? __slice.call(arguments, 1) : [];
-      console.log('published: ' + type);
       if (!this.events[type]) {
         return false;
       }
@@ -34,12 +33,24 @@
   TagAcumulator = (function() {
     function TagAcumulator(ch) {
       this.channel = ch;
-      this.channel.subscribe('store', this.store, this);
-      this.channel.subscribe('delete-last', this.delete_last, this);
+      this.channel.subscribe('accumulator:store', this.store, this);
+      this.channel.subscribe('accumulator:delete-last', this.delete_last, this);
       this.stored_tags = [];
       this.dom = this.initialize_dom();
       this.bind_events(this.dom);
     }
+    TagAcumulator.prototype.store = function(tag) {
+      this.stored_tags.push(tag);
+      return this.dom.tag_list.append(this.render(tag));
+    };
+    TagAcumulator.prototype.delete_last = function() {
+      var deleted;
+      deleted = this.stored_tags.pop();
+      return this.dom.tag_list.find("[data-tag=" + deleted + "]").remove();
+    };
+    TagAcumulator.prototype.delete_tag = function(tag) {
+      return this.stored_tags = _.without(this.stored_tags, tag);
+    };
     TagAcumulator.prototype.initialize_dom = function() {
       return {
         tag_list: $('#demo2-tag-list')
@@ -51,38 +62,27 @@
       return dom.tag_list.delegate('li.e-tag > a', 'click', function(e) {
         var tag_element;
         tag_element = $(this).closest('li.e-tag');
-        self.stored_tags = _.without(self.stored_tags, tag_element.attr('data-tag'));
+        self.delete_tag(tag_element.attr('data-tag'));
         return tag_element.remove();
       });
     };
     TagAcumulator.prototype.render = function(tag) {
       return $('<li/>').addClass('e-tag').attr('data-tag', tag).text(tag).append('<a href="#">x</a>');
     };
-    TagAcumulator.prototype.store = function(tag) {
-      this.stored_tags.push(tag);
-      return this.dom.tag_list.append(this.render(tag));
-    };
-    TagAcumulator.prototype.delete_last = function() {
-      var deleted;
-      deleted = this.stored_tags.pop();
-      return this.dom.tag_list.find("[data-tag=" + deleted + "]").remove();
-    };
     return TagAcumulator;
   })();
   TagSuggestion = (function() {
     function TagSuggestion(ch) {
       this.channel = ch;
-      this.channel.subscribe('suggest', this.suggest, this);
-      this.channel.subscribe('end-suggest', this.close_box, this);
-      this.channel.subscribe('go-down', this.go_down, this);
-      this.channel.subscribe('go-up', this.go_up, this);
-      this.channel.subscribe('select-suggestion', this.select_suggestion, this);
-      this.channel.subscribe('cancel-selection', this.cancel_selection, this);
+      this.channel.subscribe('suggest:show', this.show, this);
+      this.channel.subscribe('suggest:close', this.close, this);
+      this.channel.subscribe('suggest:next', this.go_next, this);
+      this.channel.subscribe('suggest:prev', this.go_prev, this);
+      this.channel.subscribe('suggest:select', this.select, this);
       this.input = null;
       this.selected_index = 0;
-      this.box_active = false;
-      this.suggestion_list = ["hola", "hello", "ahoy!", "bonjorno", "halo", "buenas", "akandemorl", "jarl"];
-      this.active_suggestion_list = [];
+      this.available_values = ["hola", "hello", "ahoy!", "bonjorno", "halo", "buenas", "akandemorl", "jarl"];
+      this.options_list = [];
       this.dom = this.initialize_dom();
       this.bind_events(this.dom);
     }
@@ -97,52 +97,65 @@
       return dom.suggestion_box.delegate('li.e-suggestion', 'click', function() {
         var suggestion;
         suggestion = $(this).attr('data-suggestion');
-        return self.channel.publish('selected', suggestion);
+        return self.select_suggestion(suggestion);
       });
     };
-    TagSuggestion.prototype.suggest = function(input) {
-      var s, _i, _len, _ref;
-      if (this.input !== input) {
-        this.input = input;
-        if (input.length === 0) {
-          return this.dom.suggestion_box.slideUp();
-        }
-        this.active_suggestion_list = _.filter(this.suggestion_list, function(w) {
-          return w.match(input);
+    TagSuggestion.prototype.show = function(input) {
+      var self;
+      if (this.input === input) {
+        return;
+      }
+      if (input.length === 0) {
+        return this.dom.suggestion_box.slideUp();
+      }
+      self = this;
+      this.input = input;
+      this.options_list = _.filter(this.available_values, function(w) {
+        return w.match(input);
+      });
+      if (this.options_list.length > 0) {
+        this.dom.suggestion_box.html('');
+        _.each(this.options_list, function(s) {
+          return self.dom.suggestion_box.append(self.render(s));
         });
-        if (this.active_suggestion_list.length > 0) {
-          this.dom.suggestion_box.html('');
-          _ref = this.active_suggestion_list;
-          for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-            s = _ref[_i];
-            this.dom.suggestion_box.append(this.render(s));
-          }
-          this.dom.suggestion_box.slideDown();
-          return this.selected_index = -1;
-        } else {
-          return this.dom.suggestion_box.slideUp();
-        }
+        this.dom.suggestion_box.slideDown();
+        return this.selected_index = -1;
+      } else {
+        return this.dom.suggestion_box.slideUp();
       }
     };
     TagSuggestion.prototype.render = function(s) {
       return $('<li/>').addClass('e-suggestion').attr('data-suggestion', s).text(s);
     };
-    TagSuggestion.prototype.close_box = function() {
+    TagSuggestion.prototype.close = function() {
       return this.dom.suggestion_box.hide();
     };
-    TagSuggestion.prototype.go_down = function() {};
-    TagSuggestion.prototype.go_up = function() {};
-    TagSuggestion.prototype.highlight_element = function(n) {};
-    TagSuggestion.prototype.select_suggestion = function() {};
-    TagSuggestion.prototype.cancel_selection = function() {};
+    TagSuggestion.prototype.go_next = function() {
+      this.selected_index = Math.min(this.options_list.length - 1, this.selected_index + 1);
+      return this.highlight_element();
+    };
+    TagSuggestion.prototype.go_prev = function() {
+      this.selected_index = Math.max(0, this.selected_index - 1);
+      return this.highlight_element();
+    };
+    TagSuggestion.prototype.highlight_element = function(n) {
+      n || (n = this.selected_index);
+      this.dom.suggestion_box.find('li').css('color', '');
+      return this.dom.suggestion_box.find("li:nth-child(" + (n + 1) + ")").css('color', 'red');
+    };
+    TagSuggestion.prototype.select = function(selected) {
+      selected || (selected = this.options_list[this.selected_index]);
+      this.channel.publish('suggest:selected', selected);
+      return this.close_box();
+    };
     return TagSuggestion;
   })();
   TagInput = (function() {
-    function TagInput() {
-      this.channel = new Channel();
-      this.accumulator = new TagAcumulator(this.channel);
-      this.suggest = new TagSuggestion(this.channel);
-      this.channel.subscribe('selected', this.selected_suggestion, this);
+    function TagInput(ch) {
+      this.channel = ch;
+      this.channel.subscribe('input:clear', (function() {
+        return this.dom.input.val('');
+      }), this);
       this.dom = this.initialize_dom();
       this.bind_events(this.dom);
     }
@@ -155,46 +168,56 @@
       var self;
       self = this;
       return dom.input.keyup(function(e) {
+        var value;
+        value = self.dom.input.val();
         switch (e.keyCode) {
           case 40:
-            self.channel.publish('go-down');
+            self.channel.publish('suggest:next');
             break;
           case 38:
-            self.channel.publish('go-up');
+            self.channel.publish('suggest:prev');
             break;
           case 32:
           case 188:
-            e.preventDefault();
-            self.read_input();
+            self.channel.publish('input:tag', value);
             break;
           case 13:
-            self.channel.publish('select-suggestion');
+            self.channel.publish('suggest:select');
             break;
           case 8:
-            self.channel.publish('delete-last');
-            break;
-          case 27:
-            self.channel.publish('cancel-selection');
+            if (value.length === 0) {
+              self.channel.publish('accumulator:delete-last');
+            }
         }
-        return self.channel.publish('suggest', self.dom.input.val());
+        return self.channel.publish('suggest:show', value);
       });
-    };
-    TagInput.prototype.read_input = function(input) {
-      var value;
-      value = input || this.dom.input.val();
-      value = value.trim().replace(/\W/g, '');
-      if (!(value.length <= 0)) {
-        this.channel.publish('store', value);
-      }
-      this.channel.publish('end-suggest');
-      return this.dom.input.val('');
-    };
-    TagInput.prototype.selected_suggestion = function(suggestion) {
-      return this.read_input(suggestion);
     };
     return TagInput;
   })();
+  TagWidget = (function() {
+    function TagWidget() {
+      this.channel = new Channel();
+      this.input = new TagInput(this.channel);
+      this.accumulator = new TagAcumulator(this.channel);
+      this.suggest = new TagSuggestion(this.channel);
+      this.channel.subscribe('suggest:selected', this.read_tag, this);
+      this.channel.subscribe('input:tag', this.read_tag, this);
+    }
+    TagWidget.prototype.read_tag = function(input) {
+      var value;
+      value = input.trim().replace(/\W/g, '');
+      if (!(value.length <= 0)) {
+        this.channel.publish('accumulator:store', value);
+      }
+      this.channel.publish('suggest:close');
+      return this.channel.publish('input:clear');
+    };
+    TagWidget.prototype.selected_suggestion = function(suggestion) {
+      return this.read_input(suggestion);
+    };
+    return TagWidget;
+  })();
   $(function() {
-    return window.tag_input = new TagInput();
+    return window.tag_input = new TagWidget();
   });
 }).call(this);
